fe7a1ddd6159d1cf2e0a1748a8388ea181a6aea0
Evan Darst <evan.l.darst@gmail.com>
2018-01-18 20:29:33 -0500

    created basic server initialization

---
+++ server.js
@@ -0,0 +1,18 @@
+//Dependencies
+
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+//Initialize Express and establish port
+
+var app = express();
+var PORT = 3000;
+
+//Initialize JSON parsing using bodyParser
+
+
+//Require routing for HTML and API routing files
+
+
+//Initialie our server
