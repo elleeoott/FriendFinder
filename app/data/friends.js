6d2a0635bed36faa989d991c2c99b4ab289d5e59
Evan Darst <evan.l.darst@gmail.com>
2018-01-19 07:58:22 -0500

    tweak to scores in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,19 +2,19 @@ var friends = [
 	{
 		name: "Steven Gerrard",
 		photo: "http://bit.ly/2mPjw3x",
-		scores: []
+		scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 	},{
 		name: "Thierry Henry",
 		photo: "http://bit.ly/2DmN5Vi",
-		scores: []
+		scores: [2, 3, 3, 2, 5, 5, 1, 4, 2, 1]
 	},{
 		name: "Robbie Fowler",
 		photo: "http://bit.ly/2rmPYjl",
-		scores: []
+		scores: [5, 4, 5, 5, 3, 5, 4, 5, 5, 3]
 	},{
 		name: "Roberto Firmino",
 		photo: "http://bit.ly/2rmPYjl",
-		scores: []
+		scores: [3, 2, 4, 5, 1, 2, 1, 1, 4, 5]
 	}];
 
 module.exports = friends;
