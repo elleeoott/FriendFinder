5c972512009fc52dab035bd4b70ce7042f151167
Evan Darst <evan.l.darst@gmail.com>
2018-01-18 20:24:02 -0500

    added placeholder friends stored in variable

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: "Steven Gerrard",
+		photo: "http://bit.ly/2mPjw3x",
+		scores: []
+	},{
+		name: "Thierry Henry",
+		photo: "http://bit.ly/2DmN5Vi",
+		scores: []
+	},{
+		name: "Robbie Fowler",
+		photo: "http://bit.ly/2rmPYjl",
+		scores: []
+	},{
+		name: "Roberto Firmino",
+		photo: "http://bit.ly/2rmPYjl",
+		scores: []
+	}];
+
+module.exports = friends;
